6fbc97aeec659b0c554a3f28483273ba4560c945
Michael R. Lawrence <mike@michaellawrenceit.com>
2014-10-09 12:08:05 -0400

    maintainace script to mirror down repo

--- mirror-to-labs.sh
+++ mirror-to-labs.sh
@@ -1,8 +1,8 @@
 ### This is set-up for making a public layman for testing by non spike-  general Gentoo use Abuse and Etc..
 ### you can always edit this to keep your own fork In sync, 
-##git remote set-url --push origin https://github.com/Sabayon-Labs/spike-overlay/.git  git@github.com:Spike-Pentesting/App-witchcraft.git
-git remote add origin https://github.com/Sabayon-Labs/App-witchcraft.git
-git remote add upstream https://github.com/Spike-Pentesting/App-witchcraft.git
+##git remote set-url --push origin https://github.com/Sabayon-Labs/spike-overlay.git 
+git remote add origin https://github.com/Sabayon-Labs/spike-overlay.git
+git remote add upstream https://github.com/Sabayon-Labs/spike-overlay.git
 git checkout master
 git fetch upstream
 git merge upstream/master
