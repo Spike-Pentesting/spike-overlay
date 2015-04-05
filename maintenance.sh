e6d76d9e5ffbfb697f91d01d39cc311213189031
Michael R. Lawrence <mike@michaellawrenceit.com>
2015-04-05 18:35:28 -0400

    clean up for pull request

--- maintenance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#and3k-sunrise on bitbucket some items for repo maintenance borrowed 
-
-egencache --repo spike-community-overlay --update-use-local-desc $@
-egencache --repo spike-community-overlay --update-manifests --sign-manifests=y $@
